de9573ab896710f88d787cc5a09b4fc7c6d3da15
Jean-Marie Lator <84500615+Bescore@users.noreply.github.com>
2022-11-07 16:30:47 +0100

    import de trop, ctrl+shift+o

--- InvoicesContact/src/main/java/controller/Recherche.java
+++ InvoicesContact/src/main/java/controller/Recherche.java
@@ -1,6 +1,7 @@
 package controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -9,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import dao.ClientDao;
-import dao.TypeDao;
 import modele.Clients;
 import modele.GetCookie;
 
